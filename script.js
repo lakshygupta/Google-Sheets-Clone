8be56bb4a5ae406e5057f80fefe47e786c6fe81b
Lakshy Gupta <lakshygupta99@gmail.com>
2023-06-22 02:06:01 +0530

    Cell Font functionalities added

--- script.js
+++ script.js
@@ -44,9 +44,75 @@ $(document).ready(function () {
     $(this).toggleClass("selected");
   });
 
-  $(".input-cell").click(function () {
-    $(".input-cell.selected").removeClass("selected");
-    $(this).addClass("selected");
+  $(".input-cell").click(function (event) {
+    if (event.ctrlKey) {
+      let [rowId, colId] = getRowCol(this);
+      if (rowId > 1) {
+        let topCellSelected = $(`#row-${rowId - 1}-col-${colId}`).hasClass("selected");
+        if (topCellSelected) {
+          $(this).addClass("top-cell-selected");
+          $(`#row-${rowId - 1}-col-${colId}`).addClass("bottom-cell-selected");
+        }
+      }
+      if (rowId < 100) {
+        let bottomCellSelected = $(`#row-${rowId + 1}-col-${colId}`).hasClass("selected");
+        if (bottomCellSelected) {
+          $(this).addClass("bottom-cell-selected");
+          $(`#row-${rowId + 1}-col-${colId}`).addClass("top-cell-selected");
+        }
+      }
+      if (colId > 1) {
+        let leftCellSelected = $(`#row-${rowId}-col-${colId - 1}`).hasClass("selected");
+        if (leftCellSelected) {
+          $(this).addClass("left-cell-selected");
+          $(`#row-${rowId}-col-${colId - 1}`).addClass("right-cell-selected");
+        }
+      }
+      if (colId < 100) {
+        let rightCellSelected = $(`#row-${rowId}-col-${colId + 1}`).hasClass("selected");
+        if (rightCellSelected) {
+          $(this).addClass("right-cell-selected");
+          $(`#row-${rowId}-col-${colId + 1}`).addClass("left-cell-selected");
+        }
+      }
+      $(this).addClass("selected");
+    } else {
+      $(".input-cell.selected").removeClass([
+        "selected",
+        "top-cell-selected",
+        "left-cell-selected",
+        "right-cell-selected",
+        "bottom-cell-selected",
+      ]);
+      $(this).addClass("selected");
+    }
+    // bold class add remove
+    if ($(this).hasClass("bold-selected")) {
+      $(".icon-bold").addClass("selected");
+    } else {
+      $(".icon-bold").removeClass("selected");
+    }
+
+    // italic class add remove
+    if ($(this).hasClass("italic-selected")) {
+      $(".icon-italic").addClass("selected");
+    } else {
+      $(".icon-italic").removeClass("selected");
+    }
+
+    // underline class add remove
+    if ($(this).hasClass("underline-selected")) {
+      $(".icon-underline").addClass("selected");
+    } else {
+      $(".icon-underline").removeClass("selected");
+    }
+
+    // strike through class add remove
+    if ($(this).hasClass("strike-selected")) {
+      $(".icon-strike").addClass("selected");
+    } else {
+      $(".icon-strike").removeClass("selected");
+    }
   });
 
   $(".input-cell").dblclick(function () {
@@ -54,6 +120,10 @@ $(document).ready(function () {
     $(this).addClass("selected");
     $(this).attr("contenteditable", "true");
     $(this).focus();
+  });
+
+  $(".input-cell").blur(function () {
+    $(".input-cell.selected").attr("contenteditable", false);
   });
 
   $(".input-cell-container").scroll(function () {
@@ -73,4 +143,69 @@ $(document).ready(function () {
       contents = $(this).html();
     }
   });
+});
+
+function getRowCol(thisElement) {
+  let idArray = $(thisElement).attr("id").split("-");
+  let rowId = parseInt(idArray[1]);
+  let colId = parseInt(idArray[3]);
+  return [rowId, colId];
+}
+
+function updateCell(property, value) {
+  $(".input-cell.selected").each(function () {
+    $(this).css(property, value);
+  });
+}
+
+// bold class add remove
+$(".icon-bold").click(function () {
+  if ($(this).hasClass("selected")) {
+    $(".input-cell.selected").removeClass("bold-selected");
+    updateCell("font-weight", "");
+  } else {
+    $(".input-cell.selected").addClass("bold-selected");
+    updateCell("font-weight", "bold");
+  }
+});
+
+// italics class add remove
+$(".icon-italic").click(function () {
+  if ($(this).hasClass("selected")) {
+    $(".input-cell.selected").removeClass("italic-selected");
+    updateCell("font-style", "");
+  } else {
+    $(".input-cell.selected").addClass("italic-selected");
+    updateCell("font-style", "italic");
+  }
+});
+
+// underline class add remove
+$(".icon-underline").click(function () {
+  if ($(this).hasClass("selected")) {
+    $(".input-cell.selected").removeClass("underline-selected");
+    updateCell("text-decoration", "");
+  } else {
+    $(".input-cell.selected").addClass("underline-selected");
+    updateCell("text-decoration", "underline");
+    if ($(".input-cell.selected").hasClass("strike-selected")) {
+      $(".input-cell.selected").removeClass("strike-selected");
+      $(".icon-strike").removeClass("selected");
+    }
+  }
+});
+
+// strike through class add remove
+$(".icon-strike").click(function () {
+  if ($(this).hasClass("selected")) {
+    $(".input-cell.selected").removeClass("strike-selected");
+    updateCell("text-decoration", "");
+  } else {
+    $(".input-cell.selected").addClass("strike-selected");
+    updateCell("text-decoration", "line-through");
+    if ($(".input-cell.selected").hasClass("underline-selected")) {
+      $(".input-cell.selected").removeClass("underline-selected");
+      $(".icon-underline").removeClass("selected");
+    }
+  }
 });
